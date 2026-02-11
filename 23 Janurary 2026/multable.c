ae4040d38b6bff149255c33585bdc692cd619d39
soorzj <166585708+soorzj@users.noreply.github.com>
2026-02-11 21:19:30 +0530

    Fix formatting and cleanup multiplication table code

--- 23 Janurary 2026/multable.c
+++ 23 Janurary 2026/multable.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <windows.h>
 int main() {
 int n;
 printf("enter the number of which the multiplication table is to  printed:\n");
 scanf("%d",&n);
 printf("the multiplication table of %d is\n",n);    
 for(int i=1; i<=10; i++)
-    {printf(" %d x %d = %d\n",i,n,i*n);} 
-        
-        
-    }
+    {
+        printf(" %d x %d = %d\n",i,n,i*n);
+    } 
+}
