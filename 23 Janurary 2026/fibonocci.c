cf99ac0e667ad89fe7d0fd5443e1bc865fb0294d
soorzj <166585708+soorzj@users.noreply.github.com>
2026-02-12 02:45:04 +0530

    Fix Fibonacci series calculation logic

--- 23 Janurary 2026/fibonocci.c
+++ 23 Janurary 2026/fibonocci.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 int main() {
-int n,a=1,b=0,sum=0;
+int n,a=1,b=0,c=0;
 printf("input the number of of terms to print\n");
 scanf("%d",&n);
 printf("the fibonocci series upto %d terms is:\n",n);
 while(n>0) {
-    printf("%d\n",sum);
+    printf("%d\n",c);
     b=a;
-    a=sum;
-    sum=a+b;
+    a=c;
+    c=a+b;
     n=n-1;
 }
+
 
 
 }
