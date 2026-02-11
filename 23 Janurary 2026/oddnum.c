5a3ecc63b2249d3169dc066211ab7116bd9b74ea
soorzj <166585708+soorzj@users.noreply.github.com>
2026-02-11 21:15:59 +0530

    Change output format of odd numbers to tab-separated

--- 23 Janurary 2026/oddnum.c
+++ 23 Janurary 2026/oddnum.c
@@ -6,7 +6,7 @@ int main() {
     scanf("%d", &n);
     printf("Odd numbers from 1 to %d are:\n", n);
     for (i = 1; i <= n; i += 2) 
-        printf("%d ", i);
+        printf("%d \t", i);
     printf("\n");
     return 0;
 }
