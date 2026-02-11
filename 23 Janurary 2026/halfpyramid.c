5b295e369a577191102d0d537bb892a21b89393c
soorzj <166585708+soorzj@users.noreply.github.com>
2026-02-12 02:56:56 +0530

    Fix formatting and structure of halfpyramid.c

--- 23 Janurary 2026/halfpyramid.c
+++ 23 Janurary 2026/halfpyramid.c
@@ -5,12 +5,9 @@ printf("enter the number of levels of pyramid\n");
 scanf("%d",&n);
 
 for(int i=1; i<=n; i++) {
-   
-    for(int j=1;j<=i;j++) {
-    printf("*"); } 
-    
+    for(int j=1;j<=i;j++) 
+       printf("*");  
     printf("\n");
-    }
-
+   }
     return(0);
 }
